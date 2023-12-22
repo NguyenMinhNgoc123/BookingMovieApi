dae12e6562e813756e3c94a1fabd2b0d10149b15
Ngoc <ngminhngoc123@gmail.com>
2023-12-22 10:38:37 +0700

    new commit

---
+++ BOOKING_MOVIE_MIGRATION/Migrations/20231222031453_DropForeginKey.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BOOKING_MOVIE_MIGRATION.Migrations
+{
+    public partial class DropForeginKey : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Invoice_Customer_CustomerId",
+                table: "Invoice");
+            
+            migrationBuilder.AddForeignKey(
+                name: "FK_Invoice_Customer_CustomerId",
+                table: "Invoice",
+                column: "CustomerId",
+                principalTable: "Customer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            
+        }
+    }
+}
